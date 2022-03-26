10582713c72d27681752cacc74227b8bc62e0588
rreece15 <reece.riherd@utexas.edu>
2022-03-26 12:25:04 -0500

    update & start

--- Iridium.py
+++ Iridium.py
@@ -1,1 +1,21 @@
+import time
+import board
+from adafruit_rockblock import RockBlock
 
+uart = board.UART()
+uart.baudrate = 19200
+rb = RockBlock(uart)
+
+def send_message():
+    #function
+    message = "hello world"
+
+def receive_message():
+    #function
+    message = "hello world"
+
+def process_message():
+    message = receive_message()
+
+def main():
+    main_function = 0
---
+++ WeatherBalloon.py
@@ -0,0 +1,1 @@
+#this file will combine all others and be the file that we run during launch
