bca66556d09041cd931c6044013dc8df336712eb
Maddencolin <102313627+Maddencolin@users.noreply.github.com>
2022-04-06 19:14:38 -0500

    Update Iridium.py

--- Iridium.py
+++ Iridium.py
@@ -17,8 +17,21 @@ def send_message(text_out = "hello world"):
         retry += 1
 
 def receive_message():
-    #function
-    message = "hello world"
+    # try a satellite Short Burst Data transfer
+    print("Talking to satellite...")
+    status = rb.satellite_transfer()
+    # loop as needed
+    retry = 0
+    while status[0] > 8:
+        time.sleep(10)
+        status = rb.satellite_transfer()
+        print(retry, status)
+        retry += 1
+    print("\nDONE.")
+
+    # get the text
+    print(rb.text_in)
+    return rb.text_in
 
 def process_message():
     message = receive_message()
